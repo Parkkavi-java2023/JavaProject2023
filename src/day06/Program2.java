87eb99cb38443de3d326e62cdeff14cbb9c75744
Parkkavi-java2023 <kavimani76@gmail.com>
2023-05-08 15:55:33 +0530

    Day06
    Function Overriding with static

---
+++ src/day06/Program2.java
@@ -0,0 +1,24 @@
+package day06;
+
+public class Program2 extends Program1 {
+
+	public static void main(String[] args) {
+	 	// TODO Auto-generated method stub
+Program2 p2=new Program2();
+p2.cal(10,2);
+/*Program1 p1=new Program1();
+p1.cal(15,3);*/
+	}
+	
+	/*public void cal(int a,int b)
+{
+	System.out.println("Program2 - Calculator");
+	System.out.println((a*a)+(b*b));
+	System.out.println((a*a)-(b*b));
+	System.out.println((a*a)*(b*b));
+	System.out.println((a*a)/(b*b));
+}*/
+
+	
+
+}
---
+++ src/day06/TataCarSpec.java
@@ -0,0 +1,28 @@
+package day06;
+
+public class TataCarSpec {
+	public static void displayHeight()
+	{
+		System.out.println("----");
+	}
+	
+	public void displayWidth()
+	{
+		System.out.println("----");
+	}
+	
+	public void displaySpeed()
+	{
+		System.out.println("----");
+	}
+	
+	public void displayPrice()
+	{
+		System.out.println("----");
+	}
+	
+	public void displayModelNumber()
+	{
+		System.out.println("----");	
+}
+}
