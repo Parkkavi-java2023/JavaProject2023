e40ad667bbfdd62b4fffa6b89d9160a2d36111ed
Parkkavi-java2023 <kavimani76@gmail.com>
2023-05-08 17:14:09 +0530

    Day07
    Constructor
    this and Super variables

---
+++ src/Day07/Program1.java
@@ -0,0 +1,18 @@
+package Day07;
+
+public class Program1 
+{
+	public Program1()
+	{
+		System.out.println("Testing...!");
+	}
+	public static void main(String[] args) {
+		Program1 pro = new Program1();
+		pro.display();
+	}
+	public void display()
+	{
+		System.out.println("Good Day..!");
+	}
+
+}
